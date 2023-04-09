b2100ac33ec5cfd94e02e2fb1409f968472104f2
wangyuxiang29 <wangyuxiang04@corp.netease.com>
2023-04-09 18:13:08 +0800

    feat: 完善整个卦象的页面渲染

--- miniprogram/pages/result-detail/index.ts
+++ miniprogram/pages/result-detail/index.ts
@@ -1,13 +1,23 @@
 Page({
-  onLoad() {
-    //获取云端数据库的数据
-  },
   data: {
+    desc: "",
+    name: "",
+    status: "",
     showView: false,
+    formTopIndex: [""],
+    formBottomIndex: [""],
+    property:""
+  },
+  onLoad: function (options) {
+    //获取云端数据库的数据
+    const { index:formIndex,name,status,desc,property } = options;
+    const formTopIndex =  formIndex?.split('').slice(0,3);
+    const formBottomIndex =  formIndex?.split('').slice(3);
+    console.log("详情页页面：：",formIndex)
+    this.setData({formTopIndex, formBottomIndex,name,desc,status,property})
   },
   //显示Modal
   showModal: function () {
-    console.log("showModal");
     this.setData({
       showView: true,
     });
@@ -17,6 +27,14 @@ Page({
     this.setData({
       showView: false,
     });
+  },
+  handleSubmit(){
+    this.setData({
+      showView: !this.data.showView,
+    });
+  },
+  handleSubmitGoback(){
+ wx.navigateBack();
   },
   isClearAllDraw() {
     this.setData({
--- miniprogram/pages/result/index.ts
+++ miniprogram/pages/result/index.ts
@@ -4,13 +4,19 @@ Page({
     desc: "",
     name: "",
     status: "",
-    property:''
+    property: '',
+    formIndexBottom: [""],
+    formIndexTop: [""]
   },
   onLoad: function (options) {
-    console.log(options);
-    const { formIndex, desc, name, status,property } = options;
+    
+    const { index: formIndex, desc, name, status, property } = 
+    options;
+    console.log("详情页：",formIndex)
     this.setData({
-      formIndex,
+      formIndex: formIndex,
+      formIndexTop: formIndex?.split('').slice(0, 3),
+      formIndexBottom: formIndex?.split('').slice(3),
       desc,
       name,
       status,
@@ -18,8 +24,9 @@ Page({
     });
   },
   handleSubmit() {
+    const { desc, formIndex, status, name, property } = this.data;
     wx.navigateTo({
-      url: "../result-detail/index",
+      url: `../result-detail/index?index=${formIndex}&desc=${desc}&status=${status}&name=${name}&property=${property}`,
     });
   },
 });
